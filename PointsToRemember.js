bf40f009a1131c620cfa4400fc6dbaee6f890af7
zzjjaayy <jayprimerathod@gmail.com>
2020-10-05 20:11:18 +0530

    Basic Operators for Numbers & String Methods

--- PointsToRemember.js
+++ PointsToRemember.js
@@ -13,3 +13,27 @@ multi-line comment*/
 // Console.log
 console.log("jay"); // prints the value in the browser console, usually used for 
                     // debugging and logging different values.
+
+// ESCAPE characters
+
+// 1. COMMAS INSIDE THE STRING
+// using quotes inside a string
+var str = "he said \"I will complete it tomorrow\"";
+
+// the other way is to use different quotes inside and 
+// the ones enclosing it.
+var str2 = 'he said "I will complete it tomorrow"';
+
+// another way is to ue back ticks before enclosing commas
+var str3 = `' he said "I will complete 'it' tomorrow" '`;
+// now both single and double quotes can be used inside the String.
+
+// 2. BACKSLASH INSIDE THE STRING
+// use double backslashes
+var str4 = "jay is \\amazing"; // this will show one backslash before "amazing".
+
+// 3. NEW LINE
+var str5 = "jay is \n great"; // will print "great" in a new line.
+
+// 4. TAB IN LINE
+var str6 = "jay\tgreat";
--- variables.js
+++ variables.js
@@ -10,4 +10,22 @@ y = 6;
 
 // variables can even be reassigned
 var z = 8;
+
+// Strings
 z = "jjj"
+
+// Concatenation of Strings
+var str1 = "i come last," + " always";
+// the assignment addition can also be used to concatenate
+str1 += " Awww!";
+// if a number is added to a String, the number will be considered as a String
+var str2 = "123" + 4 + 5 + 6; // should store "123456" as a String.
+
+// length of a String
+str2.length; // returns a number that is the length of the String
+
+// getting characters of a String
+var character = str[0]; // will store the first char of str2 in character.
+
+// String are immutable, that is the characters cannot be changed
+// Strings can still be replaced by a new one in variables.
