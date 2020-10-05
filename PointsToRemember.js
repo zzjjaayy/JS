5bc2ec5a3c9d32c5dcb60c158c527e7048d0493c
zzjjaayy <jayprimerathod@gmail.com>
2020-10-05 14:26:38 +0530

    Data Types

---
+++ PointsToRemember.js
@@ -0,0 +1,10 @@
+// COMMENTS
+
+// in-line comments
+/* this is a 
+multi-line comment*/
+
+// It is highly recommended to end each statement with semi-colon but not compulsory.
+// Js ignores white spaces in statements, so spaces can be given to increase 
+// readability. Usually developers prefer to give spaces and hop to the next 
+// line around operators.
---
+++ data-types.js
@@ -0,0 +1,27 @@
+// data types
+
+// variables : value can be changed {initialised with "var"}
+// constant : value cannot be changed {initialised with "const"}
+// let : just like var but with limited scope {initialised with "let"}
+
+// REMEMBER : variables in JS are dynamic meaning different data 
+// types of values can be set on the same variable.
+var a = "jayIsGreat";
+a = 9;
+
+// String - enclosed within single or double quotes
+var x = "jay";
+
+// Numbers - either integer or floats
+var y = 5;
+
+// Booleans - true or false
+var z = false;
+
+// "undefined" literally means empty {something that is not defined}
+// "null" is of value "undefined" but the data type is "object".
+// "undefined" is of value and even the data type "undefined" 
+
+// Complex Data types: 
+// 1. Objects
+// 2. Functions
